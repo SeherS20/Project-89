3d29c13caf2b3eee0d017af88aad6c1eb12538b6
SeherS20 <99300690+SeherS20@users.noreply.github.com>
2022-12-04 13:01:15 -0500

    Add files via upload

--- quiz_game_login.js
+++ quiz_game_login.js
@@ -1,9 +1,9 @@
 function addUser(){
-    Player1_name = document.getElementById("input_1").value;
-    Player2_name = document.getElementById("input_2").value;
+    player1_name = document.getElementById("input_1").value;
+    player2_name = document.getElementById("input_2").value;
 
-    localStorage.setItem("Player1_name", Player1_name)
-    localStorage.setItem("Player2_name", Player2_name)
+    localStorage.setItem("player1_name", player1_name)
+    localStorage.setItem("player2_name", player2_name)
 
     window.location = "quiz_game_page.html";
 }
